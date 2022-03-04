d11829000e99710f7f8c38525f5b6c673f7669df
Jean Fernandine <jfernandine@gmail.com>
2022-03-04 17:05:53 -0300

    injecao de dependencia

---
+++ src/main/java/com/spring/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.spring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spring.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	
+
+}
