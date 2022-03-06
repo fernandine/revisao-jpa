720fde65b88ca8b33e38e728a1df8b8a4e3cf414
Jean Fernandine <jfernandine@gmail.com>
2022-03-06 18:59:07 -0300

    Entidade Category

--- src/main/java/com/spring/config/Testconfig.java
+++ src/main/java/com/spring/config/Testconfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.spring.entities.Category;
 import com.spring.entities.Order;
 import com.spring.entities.User;
 import com.spring.entities.enums.OrderStatus;
+import com.spring.repositories.CategoryRepository;
 import com.spring.repositories.OrderRepository;
 import com.spring.repositories.UserRepository;
 
@@ -22,6 +24,8 @@ public class Testconfig implements CommandLineRunner {
 	private UserRepository userRepository;
 	@Autowired
 	private OrderRepository orderRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -34,6 +38,15 @@ public class Testconfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.CANCELED, u1);
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
+		
+		
+		
 	}
 
 	
