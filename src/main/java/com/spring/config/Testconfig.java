4e08ad9c6184bd738c34dd81d14f43d1e4baf43e
Jean Fernandine <jfernandine@gmail.com>
2022-03-09 21:00:25 -0300

    Entidade Payment

--- src/main/java/com/spring/config/Testconfig.java
+++ src/main/java/com/spring/config/Testconfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.spring.entities.Category;
 import com.spring.entities.Order;
 import com.spring.entities.OrderItem;
+import com.spring.entities.Payment;
 import com.spring.entities.Product;
 import com.spring.entities.User;
 import com.spring.entities.enums.OrderStatus;
@@ -71,7 +72,16 @@ public class Testconfig implements CommandLineRunner {
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+	
+	
+	
 	}
+	
 
 	
 }
---
+++ src/main/java/com/spring/entities/Payment.java
@@ -0,0 +1,86 @@
+package com.spring.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import net.bytebuddy.utility.dispatcher.JavaDispatcher.Instance;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Instance moment;
+	
+	@OneToOne
+	@MapsId
+	private Order order;
+	
+	public Payment(Object object, Instant instant, Order o1) {
+	}
+
+	public Payment(Long id, Instance moment, Order order) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		this.order = order;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instance getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instance moment) {
+		this.moment = moment;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Payment other = (Payment) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+
+}
